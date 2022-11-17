f88614ada4257056e05c9a9350b5d73bd1131293
Edo <edoardocecchinato.ec@gmail.com>
2022-11-17 22:39:22 +0100

    Initial structure

---
+++ server/config.py
@@ -0,0 +1,8 @@
+import os
+
+owner = os.environ['DB_OWNER']
+password_owner = os.environ['DB_OWNER_PASSWORD']
+
+db_name = os.environ['DB_NAME']
+db_ip = os.environ['DB_IP']
+db_port = os.environ['DB_PORT']
