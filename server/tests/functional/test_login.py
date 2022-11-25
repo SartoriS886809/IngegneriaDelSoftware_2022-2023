105c8596e5db047320901653274b50c34e9e2069
Mattia Dei Rossi <mattydr98@gmail.com>
2022-11-25 19:50:08 +0100

    added some tests

--- server/tests/functional/test_login.py
+++ server/tests/functional/test_login.py
@@ -1,4 +1,4 @@
-def test_login(client):
+def test_normal_login(client):
     email = "ciccio@gmail.com"
     password = "ciaociao123"
     response = client.post("/login", data={
@@ -12,7 +12,7 @@ def test_login(client):
     assert response.json["password"] == password
     assert response.json["status"] == "success"
 
-def test_login_empty_field(client):
+def test_empty_fields_login(client):
     email = ""
     password = ""
     response = client.post("/login", data={
@@ -26,8 +26,22 @@ def test_login_empty_field(client):
     assert response.json["password"] == password
     assert response.json["status"] == "failure"
     
-def test_login_incorrect_psw(client):
+def test_incorrect_psw_login(client):
     email = "email1@email.com"
+    password = "ciao"
+    response = client.post("/login", data={
+            "email": email,
+            "password": password,
+            })
+    assert response.status_code == 200
+    #print(response.json)
+    
+    assert response.json["email"] == email
+    assert response.json["password"] == password
+    assert response.json["status"] == "failure"
+
+def test_login_incorrect_email(client):
+    email = "r@gmail.com"
     password = "ciao"
     response = client.post("/login", data={
             "email": email,
---
+++ server/tests/functional/test_reports.py
@@ -0,0 +1,4 @@
+def test_view_reports(client):
+    response = client.get("/reports", data={
+        })
+    pass
