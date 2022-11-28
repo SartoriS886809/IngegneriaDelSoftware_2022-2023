0279b0201287f49dbc54052c9cde574bab430c9f
Andrea Da Lio <884046@stud.unive.it>
2022-11-28 20:33:05 +0100

    Definizione metodi di contatto

--- app_android/friendly_neighborhood/lib/configuration/configuration.dart
+++ app_android/friendly_neighborhood/lib/configuration/configuration.dart
@@ -1,4 +1,14 @@
 class Configuration {
   static const int minAge = 16;
   static const int minLengthPassword = 8;
+  /*https://blog.logrocket.com/launching-urls-flutter-url_launcher/ */
+
+  //La seguente mappa serve per definire metodi di contatto nella pagina servizi
+  static const Map<String, String> supportedContactMethods = {
+    "telefono / cellulare": "tel:\$",
+    "email": "mailto:\$",
+    "whatsapp": " https://api.whatsapp.com/send?phone=",
+    "telegram": "https://telegram.me/",
+    "sito web": "",
+  };
 }
