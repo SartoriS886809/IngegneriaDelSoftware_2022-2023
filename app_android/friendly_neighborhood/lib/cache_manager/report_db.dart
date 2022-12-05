2f7c4173d3c50f3fb67bf8220ec454351fe84dc5
Andrea Da Lio <884046@stud.unive.it>
2022-12-05 15:34:32 +0100

    Implementazione completa db report

--- app_android/friendly_neighborhood/lib/cache_manager/report_db.dart
+++ app_android/friendly_neighborhood/lib/cache_manager/report_db.dart
@@ -1,30 +1,29 @@
 // ignore_for_file: depend_on_referenced_packages
-
-import 'package:friendly_neighborhood/model/localuser.dart';
+import 'package:friendly_neighborhood/model/report.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
 //Documentazione: https://docs.flutter.dev/cookbook/persistence/sqlite
 
 //Singlenton class
-class LocalUserManager {
-  static final LocalUserManager _instance = LocalUserManager._internal();
-  static const String dbName = "localUser.db";
-  static const String tableName = "user";
+class NeedDataManager {
+  static final NeedDataManager _instance = NeedDataManager._internal();
+  static const String dbName = "needData.db";
+  static const String tableName = "need";
   late final Database _db;
   bool _isOpen = false;
 
   // using a factory is important
   // because it promises to return _an_ object of this type
   // but it doesn't promise to make a new one.
-  factory LocalUserManager() {
+  factory NeedDataManager() {
     return _instance;
   }
 
   // This named constructor is the "real" constructor
   // It'll be called exactly once, by the static property assignment above
   // it's also private, so it can only be called in this class
-  LocalUserManager._internal() {}
+  NeedDataManager._internal() {}
 
   //La funzione serve per chiudere il database
   Future close() async {
@@ -39,60 +38,60 @@ class LocalUserManager {
       //Funzione creazione database
       onCreate: (db, version) {
         return db.execute(
-            'CREATE TABLE $tableName(email TEXT PRIMARY KEY,username TEXT,name TEXT, lastname TEXT, birthdate TEXT,address TEXT, family INTEGER,houseType TEXT,neighborhood TEXT,token TEXT)');
+            'CREATE TABLE $tableName(id INTEGER PRIMARY KEY,postDate TEXT,title TEXT, priority INTEGER, category TEXT,address TEXT,creator TEXT)');
       },
       version: 1,
     );
     _isOpen = true;
   }
 
-  Future<void> insertUser(LocalUser user) async {
+  Future<void> insertReport(Report rep) async {
     if (!_isOpen) {
       open();
     }
 
     await _db.insert(
       tableName,
-      user.mapForDb(),
+      rep.toDb(),
       conflictAlgorithm: ConflictAlgorithm.replace,
     );
   }
 
-  Future<void> updateUser(LocalUser user) async {
+  Future<void> updateReport(Report rep) async {
     if (!_isOpen) {
       open();
     }
 
-    //Si può aggiornare anche la chiave primaria, perciò devo rimuovere l'utente esistente
-    LocalUser? l = await getUser();
-    if (l != null) {
-      await deleteUser(l);
-    }
-    insertUser(user);
+    await _db.update(
+      tableName,
+      rep.toDb(),
+      where: 'id = ?',
+      whereArgs: [rep.id],
+    );
   }
 
-  Future<void> deleteUser(LocalUser user) async {
+  Future<void> deleteReport(Report rep) async {
     if (!_isOpen) {
       open();
     }
 
     await _db.delete(
       tableName,
-      where: 'email = ?',
-      whereArgs: [user.email],
+      where: 'id = ?',
+      whereArgs: [rep.id],
     );
   }
 
-  Future<LocalUser?> getUser() async {
+  Future<List<Report>> getReport() async {
     // Get a reference to the database.
     if (!_isOpen) {
       open();
     }
 
     final List<Map<String, dynamic>> res = await _db.query(tableName);
-    if (res.isEmpty) return null;
-    // E' possibile avere solo 1 utente nel database, non di più
-    return LocalUser.fromDB(res[0]);
+    return List.generate(res.length, (i) {
+      return Report.fromDB(res[i]);
+    });
   }
 
   //La funzione ritorna true se il db è stato aperto, false in caso contrario
