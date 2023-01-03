59b03e77b89dc821bf7312dabc01ccc7b3a66e60
Andrea Da Lio <884046@stud.unive.it>
2023-01-03 12:44:53 +0100

    Aggiunta privacy policy

--- app_android/friendly_neighborhood/macos/Flutter/GeneratedPluginRegistrant.swift
+++ app_android/friendly_neighborhood/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,10 +7,12 @@ import Foundation
 
 import flutter_local_notifications
 import sqflite
+import syncfusion_pdfviewer_macos
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  SyncfusionFlutterPdfViewerPlugin.register(with: registry.registrar(forPlugin: "SyncfusionFlutterPdfViewerPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
